42c49d9038c0fbfd11e98163c71f2b649ae2870c
hsy0090 <hsy0090.hsy@gmail.com>
2018-01-17 16:32:54 +0800

    Update
    Replace frame(was in wrong folder),
    Added layouts except game

---
+++ FirstUnityProject/Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour {
+
+    [SerializeField]
+    public List<Canvas> CanvasList;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
