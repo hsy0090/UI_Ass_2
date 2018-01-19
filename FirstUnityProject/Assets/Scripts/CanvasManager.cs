35f2f66254c99f48fa086e784732ba96eedcff75
hsy0090 <hsy0090.hsy@gmail.com>
2018-01-20 02:33:21 +0800

    Update
    transitions,effects

--- FirstUnityProject/Assets/Scripts/CanvasManager.cs
+++ FirstUnityProject/Assets/Scripts/CanvasManager.cs
@@ -5,15 +5,40 @@ using UnityEngine;
 public class CanvasManager : MonoBehaviour {
 
     [SerializeField]
-    public List<Canvas> CanvasList;
+    public Canvas CanvasSwap;
+
+    [SerializeField]
+    public Canvas ThisCanvas;
+
+    static Canvas current;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        current = ThisCanvas;
+    }
+
+    public void SwapCanvas()
+    {
+        if(current == null)
+        {
+            current = ThisCanvas;
+        }
+
+        ThisCanvas.gameObject.SetActive(false);
+        Debug.Log("Disable curr",current);
+
+        CanvasSwap.gameObject.SetActive(true);
+        Debug.Log("Enable Canvas",current);
+
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
 }
---
+++ FirstUnityProject/Assets/Scripts/Interactions.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Interactions : MonoBehaviour {
+
+    [SerializeField]
+    GameObject panel;
+
+    [SerializeField]
+    GameObject other;
+    [SerializeField]
+    GameObject other1;
+
+    GameObject current;
+
+    // Use this for initialization
+    void Start () {
+        if (panel)
+        {
+            current = panel;
+        }
+
+        if (other)
+            other.SetActive(false);
+        if(other1)
+            other1.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void swap()
+    {
+        if (current == null)
+        {
+            current = panel;
+        }
+
+        if(other)
+            other.SetActive(false);
+
+        if(other1)
+            other1.SetActive(false);
+
+        current.gameObject.SetActive(true);
+    }
+
+    public void show()
+    {
+        if (other)
+            other.SetActive(true);
+        if (other1)
+            other1.SetActive(true);
+    }
+}
